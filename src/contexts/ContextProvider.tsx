178db4834d908a08450b55f6f437961abaa2b7b3
kvenus-uj <alexstepan0209@gmail.com>
2024-01-07 13:00:14 +0300

    first deploy

---
+++ src/contexts/ContextProvider.tsx
@@ -0,0 +1,63 @@
+import { WalletAdapterNetwork, WalletError } from '@solana/wallet-adapter-base';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider as ReactUIWalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import {
+    PhantomWalletAdapter,
+    SolflareWalletAdapter,
+    SolletExtensionWalletAdapter,
+    SolletWalletAdapter,
+    TorusWalletAdapter,
+    // LedgerWalletAdapter,
+    // SlopeWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+import { FC, ReactNode, useCallback, useMemo } from 'react';
+import { AutoConnectProvider, useAutoConnect } from './AutoConnectProvider';
+import { notify } from "../utils/notifications";
+
+const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+    const { autoConnect } = useAutoConnect();
+    const network = WalletAdapterNetwork.Mainnet;
+    // const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+    const endpoint = "https://skilled-withered-valley.solana-mainnet.quiknode.pro/e4bf9a06b4815a3d3c12ff2707da9f3a07003b02/";
+
+    console.log("endpoint", endpoint);
+
+    const wallets = useMemo(
+        () => [
+            new PhantomWalletAdapter(),
+            new SolflareWalletAdapter(),
+            new SolletWalletAdapter({ network }),
+            new SolletExtensionWalletAdapter({ network }),
+            new TorusWalletAdapter(),
+            // new LedgerWalletAdapter(),
+            // new SlopeWalletAdapter(),
+        ],
+        [network]
+    );
+
+    const onError = useCallback(
+        (error: WalletError) => {
+            notify({ type: 'error', message: error.message ? `${error.name}: ${error.message}` : error.name });
+            console.error(error);
+        },
+        []
+    );
+
+    return (
+        // TODO: updates needed for updating and referencing endpoint: wallet adapter rework
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} onError={onError} autoConnect={autoConnect}>
+                <ReactUIWalletModalProvider>{children}</ReactUIWalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
+};
+
+export const ContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+    return (
+        <AutoConnectProvider>
+            <WalletContextProvider>{children}</WalletContextProvider>
+        </AutoConnectProvider>
+    );
+};
