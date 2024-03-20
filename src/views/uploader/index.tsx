2be72aa4f9c7a007d94b8e950ded258f95133b60
jun <54099102+jun786290704@users.noreply.github.com>
2024-03-20 17:50:10 +0800

    Update index.tsx

--- src/views/uploader/index.tsx
+++ src/views/uploader/index.tsx
@@ -9,7 +9,7 @@ export const UploaderView: FC = ({ }) => {
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
         <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
-          更新元数据
+          上传元数据
         </h1>      
         <div className="text-center">
           <UploadMetadata />
