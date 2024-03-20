7c7162c4c3b6884a1b536bb04bf432fef69ba090
jun <54099102+jun786290704@users.noreply.github.com>
2024-03-20 17:36:11 +0800

    Update index.tsx
    中文

--- src/views/update/index.tsx
+++ src/views/update/index.tsx
@@ -9,7 +9,7 @@ export const UpdateView: FC = ({ }) => {
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
         <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
-          Update Metadata
+          上传元数据
         </h1>      
         <div className="text-center">
           <UpdateMetadata />
